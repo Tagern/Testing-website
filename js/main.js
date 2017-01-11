52d302f6fb7af9debc12339fc4f46828121f73f0
Jamie Millman <jamiemillman1992@hotmail.co.uk>
2017-01-11 20:03:17 +0000

    This is to the testing branch

--- js/main.js
+++ js/main.js
@@ -44,3 +44,142 @@ function returnEdited2(number){
 
 returnExample = returnEdited2(10);
 console.log(returnExample);
+
+
+// Switch statement
+function switchTest(value){
+	switch(value){
+		case 1:
+			return "number 1";
+			break;
+		case 2:
+			return "number 2";
+			break;
+		case 3:
+			return "number 3";
+			break;
+		case 4:
+			return "number 4";
+			break;
+	}
+}
+switchTest(1);
+
+// exits function after calling return value
+
+var returnTest;
+
+function returnExit(){
+	console.log("Bad mix");
+	var wonderMan = "1337";
+	var wonderWoman = "Yo";
+	return[wonderMan, wonderWoman];
+	console.log("WILL THIS SHOW!?")
+}
+
+returnTest = returnExit();
+
+console.log(returnTest[0]);
+
+//JSON
+
+var myOeJson = [
+	{
+		"name": "Jamie Millman",
+		"age": 24,
+		"favourite_anime": "FMA Brotherhood",
+		"favourite_word": "arooo",
+		"friends":["Jemma", "Tk", "Spray"]
+	}
+];
+
+console.log(myOeJson[0].friends[1])
+
+//for loop pushing into empty array
+
+var exampleArray = [];
+
+for(i=0; i<10; i++){
+	exampleArray.push(i);
+}
+
+console.log(exampleArray);
+
+
+//adding numbers in array with for loop
+
+var arrayTotal = 0;
+
+var numberArray = [10,20,30,40,50];
+
+for(i=0; i<numberArray.length; i++){
+	arrayTotal += numberArray[i];
+}
+console.log(arrayTotal);
+
+
+// cycling through an array within an array with a for loop
+
+var total = 0;
+
+function addingArray(array){
+	for(i=0; i<array.length; i++){
+		for(j=0; j<array[i].length; j++){
+			total += array[i][j];
+		}
+	}
+}
+addingArray([[1],[2],[3]]);
+
+console.log(total);
+
+
+// Cycle through array and object to check for specific things
+
+var answer = "";
+var people = [
+{
+	"firstName": "Jamie",
+	"lastName": "Millman",
+	"favouriteAnime": "Brotherhood",
+	"favouriteFilm": "Memento",
+	"partner": "Jemma Mix"
+},
+{
+	"firstName": "Jemma",
+	"lastName": "Ktorou",
+	"favouriteAnime": "One Piece",
+	"favouriteFilm": "Beauty and the Beast",
+	"partner": "Jamie Mix"
+}];
+
+function lookForAttributes(lastName, prop){
+	for(i=0; i<people.length; i++){
+		if(people[i].lastName === lastName){
+			if(people[i].hasOwnProperty(prop)){
+				return people[i][prop];
+			}
+			else{
+				return "That person doesn't have that trait";
+			}
+		}
+	}
+	return "That person doesn't exist";
+}
+
+
+answer = lookForAttributes("Ktorou", "favouriteAnime");
+console.log(answer);
+
+
+
+/////////////////////////////////////////////////////
+////////////////////  jQuery ////////////////////////
+/////////////////////////////////////////////////////
+
+$(document).ready(function(){
+
+});
+
+
+  
